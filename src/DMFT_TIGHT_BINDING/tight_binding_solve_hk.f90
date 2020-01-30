918d2cc495f4a659ba4cff48a59e290773ee3dde
Adriano Amaricci <adriano.amaricci@gmail.com>
2020-01-30 16:43:16 +0100

    1.2.13 UPDATE:
    The TB_solve_model alogn path has been updated again.
    - Updates extended to the H(r,k) hybrid form case.
    - Simplified plotting script using the GNUPLOT every statement to
    plot all blocks or a single one (corresponding to one or more
    lattice-spin-orbitals). A comment explains how to use it, just in case.
    - Added iproject flag (optional) to actually  project the k-path onto
    the lattice basis. This MUST be used if the path is defined in absolute
    terms and not with respect to the basis, then a projection is required.
    Default: iproject = TB_w90%status
    so if W90 is used projection is automatic and must be disabled explicitly.

--- src/DMFT_TIGHT_BINDING/tight_binding_solve_hk.f90
+++ src/DMFT_TIGHT_BINDING/tight_binding_solve_hk.f90
@@ -1,4 +1,4 @@
-subroutine solve_Hk_along_BZpath(hk_model,Nlso,kpath,Nk,colors_name,points_name,file)
+subroutine solve_Hk_along_BZpath(hk_model,Nlso,kpath,Nkpath,colors_name,points_name,file,iproject)
   interface 
      function hk_model(kpoint,N)
        real(8),dimension(:)      :: kpoint
@@ -8,38 +8,47 @@ subroutine solve_Hk_along_BZpath(hk_model,Nlso,kpath,Nk,colors_name,points_name,
   end interface
   integer                                   :: Nlso
   real(8),dimension(:,:)                    :: kpath
-  integer                                   :: Nk
+  integer                                   :: Nkpath
   type(rgb_color),dimension(Nlso)           :: colors_name
   character(len=*),dimension(size(kpath,1)) :: points_name
   character(len=*),optional                 :: file
-  character(len=256)                        :: file_,xtics
-  integer                                   :: Npts,Nrot,Ndim
-  integer                                   :: ipts,ik,ic,unit,u1,u2,iorb
+  logical,optional                          :: iproject
+  character(len=256)                        :: file_
+  logical                                   :: iproject_
+  character(len=256)                        :: xtics
+  integer                                   :: Npts,Ndim,Nktot
+  integer                                   :: ipts,ik,ic,unit,iorb
   real(8),dimension(size(kpath,2))          :: kstart,kstop,kpoint,kdiff
   real(8)                                   :: eval(Nlso),coeff(Nlso),klen,ktics(size(Kpath,1))
-  complex(8)                                :: h(Nlso,Nlso),u(Nlso,Nlso)
+  complex(8)                                :: h(Nlso,Nlso)
   type(rgb_color)                           :: corb(Nlso),c(Nlso)
   character(len=10)                         :: chpoint
   character(len=32)                         :: fmt
   real(8),allocatable                       :: kseg(:),Ekval(:,:)
   integer,allocatable                       :: Ekcol(:,:)
-
-  file_="Eigenbands.tb";if(present(file))file_=file
-  Npts=size(kpath,1)
-  Ndim=size(kpath,2)
+  !
+  file_    = "Eigenbands.tb";if(present(file))file_=file
+  iproject_= .false.        ;if(present(iproject))iproject_=iproject
+  !
+  Npts = size(kpath,1)
+  Ndim = size(kpath,2)
+  Nktot= (Npts-1)*Nkpath
   do iorb=1,Nlso
      corb(iorb) = colors_name(iorb)
   enddo
   !
   if(.not.set_bkvec)stop "solve_w90hk_along_BZpath ERROR: bk vectors not set!"
-  select case(Ndim)
-  case (1)
-     forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x
-  case(2)
-     forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x + kpath(ipts,2)*bk_y
-  case (3)
-     forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x + kpath(ipts,2)*bk_y + kpath(ipts,3)*bk_z
-  end select
+  !
+  if(iproject)then
+     select case(Ndim)
+     case (1)
+        forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x
+     case(2)
+        forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x + kpath(ipts,2)*bk_y
+     case (3)
+        forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x + kpath(ipts,2)*bk_y + kpath(ipts,3)*bk_z
+     end select
+  endif
   !
   !
   write(*,*)"Solving model along the path:"
@@ -49,18 +58,16 @@ subroutine solve_Hk_along_BZpath(hk_model,Nlso,kpath,Nk,colors_name,points_name,
   enddo
   !
   ic = 0
-  allocate(kseg(Nk*(Npts-1)))
-  allocate(ekval(Nk*(Npts-1),Nlso))
-  allocate(ekcol(Nk*(Npts-1),Nlso))
-  ! unit=free_unit()
-  ! open(unit,file=reg(file_))
+  allocate(kseg(Nktot))
+  allocate(ekval(Nktot,Nlso))
+  allocate(ekcol(Nktot,Nlso))
   klen=0d0  
   do ipts=1,Npts-1
      kstart = kpath(ipts,:)
      kstop  = kpath(ipts+1,:)
-     kdiff  = (kstop-kstart)/dble(Nk)
+     kdiff  = (kstop-kstart)/dble(Nkpath)
      ktics(ipts)  = klen
-     do ik=1,Nk
+     do ik=1,Nkpath
         ic=ic+1
         kpoint = kstart + (ik-1)*kdiff
         h = hk_model(kpoint,Nlso)
@@ -72,17 +79,15 @@ subroutine solve_Hk_along_BZpath(hk_model,Nlso,kpath,Nk,colors_name,points_name,
            Ekcol(ic,iorb) = rgb(c(iorb))
         enddo
         kseg(ic) = klen
-        ! write(unit,'(F18.12,100(F18.12,I18))')klen,(Eval(iorb),rgb(c(iorb)),iorb=1,Nlso)
         klen = klen + sqrt(dot_product(kdiff,kdiff))
      enddo
   enddo
   ktics(Npts) = kseg(ic-1)
-  ! close(unit)
   !
   unit=free_unit()
   open(unit,file=reg(file_))
   do iorb=1,Nlso
-     do ic=1,Nk*(Npts-1)
+     do ic=1,Nktot
         write(unit,*)kseg(ic),Ekval(ic,iorb),Ekcol(ic,iorb)
      enddo
      write(unit,*)""
@@ -108,170 +113,42 @@ subroutine solve_Hk_along_BZpath(hk_model,Nlso,kpath,Nk,colors_name,points_name,
   write(unit,*)"set xtics ("//reg(xtics)//")"
   write(unit,*)"set grid noytics xtics"
   !
-  if(Nlso<10)then !a safe compromise to have all orbitals key readable.
-     do iorb=1,Nlso
-        chpoint=str(0.95d0-(iorb-1)*0.05d0)
-        write(unit,"(A)")str("set label 'Orb "//str(iorb)//"' tc rgb "//str(rgb(corb(iorb)))//&
-             " at graph 0.9,"//reg(chpoint)//" font 'Times-Italic,11'")
-     enddo
-  else
-     do iorb=1,Nlso
-        chpoint=str(0.95d0-(iorb-1)*0.05d0)
-        write(unit,"(A)")str("#set label 'Orb "//str(iorb)//"' tc rgb "//str(rgb(corb(iorb)))//&
-             " at graph 0.9,"//reg(chpoint)//" font 'Times-Italic,11'")
-     enddo
-  endif
+  do iorb=1,Nlso
+     chpoint=str(0.95d0-(iorb-1)*0.05d0)
+     write(unit,"(A)")str("#set label 'Orb "//str(iorb)//"' tc rgb "//str(rgb(corb(iorb)))//&
+          " at graph 0.9,"//reg(chpoint)//" font 'Times-Italic,11'")
+  enddo
   !
-  write(unit,*)"plot '"//reg(file_)//"' u 1:2:3 w l lw 3 lc rgb variable" !,\"
-  ! do iorb=2,Nlso-1
-  !    u1=2+(iorb-1)*2
-  !    u2=3+(iorb-1)*2
-  !    write(unit,*)"'"//reg(file_)//"' u 1:"&
-  !         //reg(txtfy(u1))//":"//reg(txtfy(u2))//" w l lw 3 lc rgb variable,\"
-  ! enddo
-  ! u1=2+(Nlso-1)*2
-  ! u2=3+(Nlso-1)*2
-  ! write(unit,*)"'"//reg(file_)//"' u 1:"&
-  !      //reg(txtfy(u1))//":"//reg(txtfy(u2))//" w l lw 3 lc rgb variable"
-  ! !
+  write(unit,*)"plot '"//reg(file_)//"' every :::0 u 1:2:3 w l lw 3 lc rgb variable"
+  write(unit,*)"# to print from the i-th to the j-th block use: every :::i::j"
+  !
   close(unit)
   call system("chmod +x "//reg(file_)//".gp")
 end subroutine solve_Hk_along_BZpath
 
-
-
-subroutine solve_w90Hk_along_BZpath(Nlso,kpath,Nk,colors_name,points_name,file)
+subroutine solve_w90Hk_along_BZpath(Nlso,kpath,Nkpath,colors_name,points_name,file,iproject)
   integer                                   :: Nlso
   real(8),dimension(:,:)                    :: kpath
-  integer                                   :: Nk
+  integer                                   :: Nkpath
   type(rgb_color),dimension(Nlso)           :: colors_name
   character(len=*),dimension(size(kpath,1)) :: points_name
   character(len=*),optional                 :: file
-  character(len=256)                        :: file_,xtics
-  integer                                   :: Npts,Nrot,Ndim
-  integer                                   :: ipts,ik,ic,unit,u1,u2,iorb
-  real(8),dimension(size(kpath,2))          :: kstart,kstop,kpoint,kdiff
-  real(8)                                   :: eval(Nlso),coeff(Nlso),klen,ktics(size(Kpath,1))
-  complex(8)                                :: h(Nlso,Nlso),u(Nlso,Nlso)
-  type(rgb_color)                           :: corb(Nlso),c(Nlso)
-  character(len=10)                         :: chpoint
-  character(len=32)                         :: fmt
-  real(8),allocatable                       :: kseg(:),Ekval(:,:)
-  integer,allocatable                       :: Ekcol(:,:)
+  logical,optional                          :: iproject
+  character(len=256)                        :: file_
+  logical                                   :: iproject_
+  character(len=256)                        :: xtics
   !
   if(.not.TB_w90%status)stop "solve_w90hk_along_BZpath: TB_w90 structure not allocated. Call setup_w90 first."
+  file_    = "Eigenbands.tb";if(present(file))file_=file
+  iproject_= .false.        ;if(present(iproject))iproject_=iproject
   !
-  file_="Eigenbands.tb";if(present(file))file_=file
-  Npts=size(kpath,1)
-  Ndim=size(kpath,2)
-  do iorb=1,Nlso
-     corb(iorb) = colors_name(iorb)
-  enddo
+  call solve_Hk_along_BZpath(w90_hk_model,Nlso,kpath,Nkpath,colors_name,points_name,file_,iproject_)
   !
-  if(.not.set_bkvec)stop "solve_w90hk_along_BZpath ERROR: bk vectors not set!"
-  select case(Ndim)
-  case (1)
-     forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x
-  case(2)
-     forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x + kpath(ipts,2)*bk_y
-  case (3)
-     forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x + kpath(ipts,2)*bk_y + kpath(ipts,3)*bk_z
-  end select
-  !
-  !
-  write(*,*)"Solving model along the path:"
-  write(fmt,"(A3,I0,A)")"(A,",size(kpath,2),"F7.4,A1)"
-  do ipts=1,Npts
-     write(*,fmt)"Point"//str(ipts)//": [",(kpath(ipts,ic),ic=1,size(kpath,2)),"]"
-  enddo
-  !
-  ic = 0
-  allocate(kseg(Nk*(Npts-1)))
-  allocate(ekval(Nk*(Npts-1),Nlso))
-  allocate(ekcol(Nk*(Npts-1),Nlso))
-  ! unit=free_unit()
-  ! open(unit,file=reg(file_))
-  klen=0d0  
-  do ipts=1,Npts-1
-     kstart = kpath(ipts,:)
-     kstop  = kpath(ipts+1,:)
-     kdiff  = (kstop-kstart)/dble(Nk)
-     ktics(ipts)  = klen
-     do ik=1,Nk
-        ic=ic+1
-        kpoint = kstart + (ik-1)*kdiff
-        !
-        h = w90_hk_model(kpoint,Nlso)
-        !
-        call eigh(h,Eval)
-        do iorb=1,Nlso
-           coeff(:)=h(:,iorb)*conjg(h(:,iorb))
-           c(iorb) = coeff.dot.corb
-           Ekval(ic,iorb) = Eval(iorb)
-           Ekcol(ic,iorb) = rgb(c(iorb))
-        enddo
-        kseg(ic) = klen
-        ! write(unit,'(F18.12,100(F18.12,I18))')klen,(Eval(iorb),rgb(c(iorb)),iorb=1,Nlso)
-        klen = klen + sqrt(dot_product(kdiff,kdiff))
-     enddo
-  enddo
-  ktics(Npts) = kseg(ic-1)
-  ! close(unit)
-  !
-  xtics="'"//reg(points_name(1))//"'"//str(ktics(1))//","
-  do ipts=2,Npts-1
-     xtics=reg(xtics)//"'"//reg(points_name(ipts))//"'"//str(ktics(ipts))//","
-  enddo
-  xtics=reg(xtics)//"'"//reg(points_name(Npts))//"'"//str(ktics(Npts))//""
-  !
-  open(unit,file=reg(file_)//".gp")
-  write(unit,*)"#set terminal pngcairo size 350,262 enhanced font 'Verdana,10'"
-  write(unit,*)"#set out '"//reg(file_)//".png'"
-  write(unit,*)""
-  write(unit,*)"#set terminal svg size 350,262 fname 'Verdana, Helvetica, Arial, sans-serif'"
-  write(unit,*)"#set out '"//reg(file_)//".svg'"
-  write(unit,*)""
-  write(unit,*)"#set term postscript eps enhanced color 'Times'"
-  write(unit,*)"#set output '|ps2pdf -dEPSCrop - "//reg(file_)//".pdf'"
-  write(unit,*)"unset key"
-  write(unit,*)"set xtics ("//reg(xtics)//")"
-  write(unit,*)"set grid noytics xtics"
-  !
-  if(Nlso<10)then !a safe compromise to have all orbitals key readable.
-     do iorb=1,Nlso
-        chpoint=str(0.95d0-(iorb-1)*0.05d0)
-        write(unit,"(A)")str("set label 'Orb "//str(iorb)//"' tc rgb "//str(rgb(corb(iorb)))//&
-             " at graph 0.9,"//reg(chpoint)//" font 'Times-Italic,11'")
-     enddo
-  else
-     do iorb=1,Nlso
-        chpoint=str(0.95d0-(iorb-1)*0.05d0)
-        write(unit,"(A)")str("#set label 'Orb "//str(iorb)//"' tc rgb "//str(rgb(corb(iorb)))//&
-             " at graph 0.9,"//reg(chpoint)//" font 'Times-Italic,11'")
-     enddo
-  endif
-  !
-  write(unit,*)"plot '"//reg(file_)//"' u 1:2:3 w l lw 3 lc rgb variable" !,\"
-  ! do iorb=2,Nlso-1
-  !    u1=2+(iorb-1)*2
-  !    u2=3+(iorb-1)*2
-  !    write(unit,*)"'"//reg(file_)//"' u 1:"&
-  !         //reg(txtfy(u1))//":"//reg(txtfy(u2))//" w l lw 3 lc rgb variable,\"
-  ! enddo
-  ! u1=2+(Nlso-1)*2
-  ! u2=3+(Nlso-1)*2
-  ! write(unit,*)"'"//reg(file_)//"' u 1:"&
-  !      //reg(txtfy(u1))//":"//reg(txtfy(u2))//" w l lw 3 lc rgb variable"
-  !
-  close(unit)
-  call system("chmod +x "//reg(file_)//".gp")
 end subroutine solve_w90Hk_along_BZpath
 
 
 
-
-
-subroutine solve_HkR_along_BZpath(hkr_model,Nlat,Nso,kpath,Nkpath,colors_name,points_name,file,pbc)
+subroutine solve_HkR_along_BZpath(hkr_model,Nlat,Nso,kpath,Nkpath,colors_name,points_name,file,pbc,iproject)
   interface 
      function hkr_model(kpoint,Nlat,Nso,pbc)
        real(8),dimension(:)                    :: kpoint
@@ -280,26 +157,31 @@ subroutine solve_HkR_along_BZpath(hkr_model,Nlat,Nso,kpath,Nkpath,colors_name,po
        complex(8),dimension(Nlat*Nso,Nlat*Nso) :: hkr_model
      end function hkr_model
   end interface
-  integer                                      :: Nlat,Nso,Nlso
-  real(8),dimension(:,:)                       :: kpath
-  integer                                      :: Nkpath,Nktot
-  type(rgb_color),dimension(Nlat,Nso)          :: colors_name
-  character(len=*),dimension(size(kpath,1))    :: points_name
-  character(len=*),optional                    :: file
-  logical                                      :: pbc
-  character(len=256)                           :: file_,xtics
-  integer                                      :: Npts,Ndim!,units(Nlat*Nso)
-  integer                                      :: ipts,ik,ic,unit,unit_,iorb,ilat,io,nrot,u1,u2
-  real(8)                                      :: coeff(Nlat*Nso),klen,ktics(size(Kpath,1))
-  type(rgb_color)                              :: corb(Nlat*Nso),c(Nlat*Nso)
-  real(8),dimension(size(kpath,2))             :: kstart,kstop,kpoint,kdiff
-  complex(8),dimension(Nlat*Nso,Nlat*Nso)      :: h
-  real(8),dimension(Nlat*Nso)                  :: Eval
-  character(len=64)                            :: fmt
+  integer                                   :: Nlat,Nso,Nlso
+  real(8),dimension(:,:)                    :: kpath
+  integer                                   :: Nkpath,Nktot
+  type(rgb_color),dimension(Nlat,Nso)       :: colors_name
+  character(len=*),dimension(size(kpath,1)) :: points_name
+  character(len=*),optional                 :: file
+  logical,optional                          :: pbc,iproject
+  character(len=256)                        :: file_
+  logical                                   :: pbc_,iproject_
+  character(len=256)                        :: xtics
+  integer                                   :: Npts,Ndim
+  integer                                   :: ipts,ik,ic,unit,iorb,ilat,io,nrot,u1,u2
+  real(8)                                   :: coeff(Nlat*Nso),klen,ktics(size(Kpath,1))
+  type(rgb_color)                           :: corb(Nlat*Nso),c(Nlat*Nso)
+  real(8),dimension(size(kpath,2))          :: kstart,kstop,kpoint,kdiff
+  complex(8),dimension(Nlat*Nso,Nlat*Nso)   :: h
+  real(8),dimension(Nlat*Nso)               :: Eval
+  real(8),allocatable                       :: kseg(:),Ekval(:,:)
+  integer,allocatable                       :: Ekcol(:,:)
+  !
+  file_    = "Eigenbands.tb";if(present(file))file_=file
+  iproject_= .false.        ;if(present(iproject))iproject_=iproject
+  pbc_     = .true.         ;if(present(pbc))pbc_=pbc
   !
   Nlso  = Nlat*Nso
-  write(fmt,"(A,I0,A)")"(I12,",Nlso,"(F18.12,I18))"
-  file_ = "Eigenbands.tb";if(present(file))file_=file
   Npts  = size(kpath,1)
   Ndim  = size(kpath,2)
   Nktot = (Npts-1)*Nkpath
@@ -311,27 +193,27 @@ subroutine solve_HkR_along_BZpath(hkr_model,Nlat,Nso,kpath,Nkpath,colors_name,po
   enddo
   !
   if(.not.set_bkvec)stop "solve_w90hk_along_BZpath ERROR: bk vectors not set!"
-  select case(Ndim)
-  case (1)
-     forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x
-  case(2)
-     forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x + kpath(ipts,2)*bk_y
-  case (3)
-     forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x + kpath(ipts,2)*bk_y + kpath(ipts,3)*bk_z
-  end select
+  !
+  if(iproject_)then
+     select case(Ndim)
+     case (1)
+        forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x
+     case(2)
+        forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x + kpath(ipts,2)*bk_y
+     case (3)
+        forall(ipts=1:Npts)kpath(ipts,:) = kpath(ipts,1)*bk_x + kpath(ipts,2)*bk_y + kpath(ipts,3)*bk_z
+     end select
+  endif
   !
   write(*,*)"Solving model along the path:"
   do ipts=1,Npts
      write(*,"(A,10(A,1x),A1)")"Point"//str(ipts)//": [",(str(kpath(ipts,ic)),ic=1,size(kpath,2)),"]"
   enddo
   !
-  do io=1,Nlso
-     open(free_unit(unit_),file="site_"//str(io,4)//"_"//str(file_))
-     rewind(unit_)
-     close(unit_)
-  enddo
-  !
   ic=0
+  allocate(kseg(Nktot))
+  allocate(ekval(Nktot,Nlso))
+  allocate(ekcol(Nktot,Nlso))
   klen = 0d0
   call start_timer()
   do ipts=1,Npts-1
@@ -344,19 +226,29 @@ subroutine solve_HkR_along_BZpath(hkr_model,Nlat,Nso,kpath,Nkpath,colors_name,po
         kpoint = kstart + (ik-1)*kdiff
         h = hkr_model(kpoint,Nlat,Nso,pbc)
         call eigh(h,Eval)
-        call eta(ic,Nktot)       
+        call eta(ic,Nktot)
         do io=1,Nlso
            coeff(:)=h(:,io)*conjg(h(:,io))
            c(io) = coeff.dot.corb
-           open(free_unit(unit_),file="site_"//str(io,4)//"_"//str(file_),position="append")
-           write(unit_,"(F18.12,F18.12,I18)")klen,Eval(io),rgb(c(io))
-           close(unit_)
+           Ekval(ic,io) = Eval(io)
+           Ekcol(ic,io) = rgb(c(io))
         enddo
+        kseg(ic) = klen
         klen = klen + sqrt(dot_product(kdiff,kdiff))
      enddo
   enddo
-  ktics(Npts) = klen
+  ktics(Npts) = Kseg(ic-1)
   call stop_timer()
+  !
+  open(free_unit(unit),file=str(file_))
+  do io=1,Nlso
+     do ic=1,Nktot
+        write(unit,*)kseg(ic),Ekval(ic,io),Ekcol(ic,io)
+     enddo
+     write(unit,*)""
+  enddo
+  close(unit)
+  !
   !
   xtics=""
   xtics="'"//reg(points_name(1))//"'"//str(ktics(1))//","
@@ -377,13 +269,15 @@ subroutine solve_HkR_along_BZpath(hkr_model,Nlat,Nso,kpath,Nkpath,colors_name,po
   write(unit,*)"unset key"
   write(unit,*)"set xtics ("//reg(xtics)//")"
   write(unit,*)"set grid ytics xtics"
-  io=1
-  write(unit,*)"plot 'site_"//reg(txtfy(io,4))//"_"//reg(file_)//"' u 1:2:3 w l lw 2 lc rgb variable,\"
-  do io=2,Nlso-1
-     write(unit,*)"'site_"//reg(txtfy(io,4))//"_"//reg(file_)//"' u 1:2:3 w l lw 2 lc rgb variable,\"
-  enddo
-  write(unit,*)"'site_"//reg(txtfy(Nlso,4))//"_"//reg(file_)//"' u 1:2:3 w l lw 2 lc rgb variable"
+  !
+  write(unit,*)"plot '"//reg(file_)//"' every :::0 u 1:2:3 w l lw 3 lc rgb variable"
+  write(unit,*)"# to print from the i-th to the j-th block use every :::i::j"
+  !
   close(unit)
+  !
   call system("chmod +x "//reg(file_)//".gp")
 end subroutine solve_HkR_along_BZpath
+
+
+
 
