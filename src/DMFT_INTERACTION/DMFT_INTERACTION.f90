4e5f5cd51061724d1fc9bccf1077af5a5f275e57
francesco.petocchi@gmail.com <francesco.petocchi@gmail.com>
2020-02-18 15:09:52 +0100

    Included new DMFT_TOOLS module where all the subroutines related to the Interaction tensor will be stored The only missing thing is are routines concerning the change of basis. Code compiles without errors.

---
+++ src/DMFT_INTERACTION/DMFT_INTERACTION.f90
@@ -0,0 +1,367 @@
+module DMFT_INTERACTION
+  USE SF_TIMER
+  USE SF_CONSTANTS, only:one,xi,zero,pi
+  USE SF_IOTOOLS,   only:reg,txtfy
+  USE SF_ARRAYS,    only:arange
+  USE SF_LINALG,    only:eye,inv,diag
+  USE SF_MISC,      only:assert_shape
+  USE SF_IOTOOLS,   only:free_unit,reg,txtfy
+  USE DMFT_CTRL_VARS
+#ifdef _MPI
+  USE SF_MPI
+  USE MPI
+#endif
+  implicit none
+  private
+
+
+  interface dmft_interaction_setKanamori
+     module procedure :: dmft_interaction_setKanamori_serial
+#ifdef _MPI
+     module procedure :: dmft_interaction_setKanamori_mpi
+#endif
+  end interface dmft_interaction_setKanamori
+
+
+!  interface dmft_interaction_rotate
+!     module procedure :: dmft_interaction_rotate_serial
+!#ifdef _MPI
+!     module procedure :: dmft_interaction_rotate_mpi
+!#endif
+!  end interface dmft_interaction_rotate
+
+
+  public :: dmft_interaction_setKanamori
+  !public :: dmft_interaction_rotate
+
+  public :: dmft_interaction_print
+  public :: dmft_interaction_read
+
+
+
+
+  !##################################################################
+  !##################################################################
+  !##################################################################
+
+
+
+  interface tensor2matrix_reshape
+     module procedure  :: d_tensor2matrix_reshape
+     module procedure  :: c_tensor2matrix_reshape
+  end interface tensor2matrix_reshape
+
+  interface matrix2tensor_reshape
+     module procedure d_matrix2tensor_reshape
+     module procedure c_matrix2tensor_reshape
+  end interface matrix2tensor_reshape
+
+
+  integer                          :: mpi_ierr
+  integer                          :: mpi_rank
+  integer                          :: mpi_size
+  logical                          :: mpi_master
+
+
+
+contains
+
+
+  !--------------------------------------------------------------------!
+  !PURPOSE: Set the rank-4 tensor to the standard Kanamori interaction
+  !--------------------------------------------------------------------!
+
+  include "dmft_interaction_setKanamori_serial.f90"
+#ifdef _MPI
+  include "dmft_interaction_setKanamori_mpi.f90"
+#endif
+
+
+  !--------------------------------------------------------------------!
+  !PURPOSE: Rotate the rank-4 tensor to a given basis
+  !--------------------------------------------------------------------!
+!  include "dmft_interaction_rotate.f90"
+!#ifdef _MPI
+!  include "dmft_interaction_rotate_mpi.f90"
+!#endif
+
+
+  !--------------------------------------------------------------------!
+  !                          INTERACTION I/O
+  !--------------------------------------------------------------------!
+  subroutine dmft_interaction_read(Utensor,file)
+    implicit none
+    real(8),dimension(:,:,:,:,:),intent(inout)  :: Utensor
+    character(len=*),intent(in)                 :: file
+    !aux
+    real(8),dimension(:,:,:),allocatable        :: Umat
+    integer                                     :: Norb
+    integer                                     :: LOGfile
+    integer                                     :: io,jo
+    integer                                     :: unit
+    !
+    call get_ctrl_var(Norb,"NORB")
+    call get_ctrl_var(LOGfile,"LOGfile")
+    !
+    call assert_shape(Utensor,[Norb,Norb,Norb,Norb,2],"dmft_interaction_read","Umat")
+    allocate(Umat(Norb*Norb,Norb*Norb,2));Umat=0d0
+    !
+    open(free_unit(unit),file=reg(file))
+    write(LOGfile,"(A)")"Read interaction tensor from file :"//reg(file)
+    !
+    read(unit,*)
+    do io=1,Norb*Norb
+      read(unit,"(90F21.12)") (Umat(io,jo,1),jo=1,Norb*Norb)
+    enddo
+    read(unit,*)
+    read(unit,*)
+    do io=1,Norb*Norb
+      read(unit,"(90F21.12)") (Umat(io,jo,2),jo=1,Norb*Norb)
+    enddo
+    !
+    Utensor=matrix2tensor_reshape(Umat,Norb)
+    !
+    deallocate(Umat)
+    !
+  end subroutine dmft_interaction_read
+
+
+
+  subroutine dmft_interaction_print(Utensor,file)
+    implicit none
+    real(8),dimension(:,:,:,:,:),intent(in)     :: Utensor
+    character(len=*),optional                   :: file
+    !aux
+    real(8),dimension(:,:,:),allocatable        :: Umat
+    integer                                     :: Norb
+    integer                                     :: LOGfile
+    integer                                     :: io,jo
+    integer                                     :: unit
+    !
+    call get_ctrl_var(Norb,"NORB")
+    call get_ctrl_var(LOGfile,"LOGfile")
+    !
+    call assert_shape(Utensor,[Norb,Norb,Norb,Norb,2],"dmft_interaction_print","Umat")
+    allocate(Umat(Norb*Norb,Norb*Norb,2));Umat=0d0
+    Umat=tensor2matrix_reshape(Utensor,Norb)
+    !
+    write(LOGfile,*)
+    write(LOGfile,"(A)")"Interaction tensor - Block (up,up)(dw,dw):"
+    call print_interaction_states(LOGfile,Norb)
+    do io=1,Norb*Norb
+       write(LOGfile,"(90F4.3)") (Umat(io,jo,1),jo=1,Norb*Norb)
+    enddo
+    write(LOGfile,*)
+    write(LOGfile,"(A)")"Interaction tensor - Block (up,up)(up,up):"
+    call print_interaction_states(LOGfile,Norb)
+    do io=1,Norb*Norb
+       write(LOGfile,"(90F4.3)") (Umat(io,jo,2),jo=1,Norb*Norb)
+    enddo
+    !
+    if(present(file))then
+      !
+       open(free_unit(unit),file=reg(file))
+       write(LOGfile,"(A)")"Print interaction tensor on file :"//reg(file)
+       !
+       call print_interaction_states(unit,Norb)
+       do io=1,Norb*Norb
+          write(unit,"(90F21.12)") (Umat(io,jo,1),jo=1,Norb*Norb)
+       enddo
+       write(unit,*)
+       call print_interaction_states(unit,Norb)
+       do io=1,Norb*Norb
+          write(unit,"(90F21.12)") (Umat(io,jo,2),jo=1,Norb*Norb)
+       enddo
+       !
+    endif
+    !
+    deallocate(Umat)
+    !
+  end subroutine dmft_interaction_print
+
+
+
+  !--------------------------------------------------------------------!
+  !                         AUXILIARY STUFF
+  !--------------------------------------------------------------------!
+  subroutine print_interaction_states(unit,Norb)
+    implicit none
+    integer,intent(in)                         :: Norb
+    integer,intent(in)                         :: unit
+    character(len=32)                          :: fmt1,fmt2
+    character(len=32)                          :: fmt
+    !
+    if(unit==6)then
+      if(Norb==3) write(unit,"(90a8)") "(11)","(22)","(33)", &
+                                       "(12)","(21)",        &
+                                       "(23)","(32)",        &
+                                       "(31)","(13)"
+      if(Norb==2) write(unit,"(90a8)") "(11)","(22)", &
+                                       "(12)","(21)"
+    else
+      if(Norb==3) write(unit,"(90a21)")"(11)","(22)","(33)", &
+                                       "(12)","(21)",        &
+                                       "(23)","(32)",        &
+                                       "(31)","(13)"
+      if(Norb==2) write(unit,"(90a21)")"(11)","(22)", &
+                                       "(12)","(21)"
+    endif
+  end subroutine print_interaction_states
+
+
+  function c_tensor2matrix_reshape(tensor,Norb) result(matrix)
+    implicit none
+    integer                                     :: Norb
+    complex(8),dimension(Norb,Norb,Norb,Norb,2) :: tensor
+    complex(8),dimension(Norb*Norb,Norb*Norb,2) :: matrix
+    integer                                     :: iorb,jorb,korb,lorb
+    integer                                     :: io,jo,spin
+    matrix = zero
+    do iorb=1,Norb
+      do jorb=1,Norb
+          do korb=1,Norb
+             do lorb=1,Norb
+                !
+                call getUstride(io,iorb,jorb,Norb)
+                call getUstride(jo,korb,lorb,Norb)
+                !
+                do spin=1,2
+                   matrix(io,jo,spin) = tensor(iorb,jorb,korb,lorb,spin)
+                enddo
+                !
+             enddo
+          enddo
+       enddo
+    enddo
+  end function c_tensor2matrix_reshape
+
+  function d_tensor2matrix_reshape(tensor,Norb) result(matrix)
+    implicit none
+    integer                                     :: Norb
+    real(8),dimension(Norb,Norb,Norb,Norb,2)    :: tensor
+    real(8),dimension(Norb*Norb,Norb*Norb,2)    :: matrix
+    integer                                     :: iorb,jorb,korb,lorb
+    integer                                     :: io,jo,spin
+    matrix = 0d0
+    do iorb=1,Norb
+      do jorb=1,Norb
+          do korb=1,Norb
+             do lorb=1,Norb
+                !
+                call getUstride(io,iorb,jorb,Norb)
+                call getUstride(jo,korb,lorb,Norb)
+                !
+                do spin=1,2
+                   matrix(io,jo,spin) = tensor(iorb,jorb,korb,lorb,spin)
+                enddo
+                !
+             enddo
+          enddo
+       enddo
+    enddo
+  end function d_tensor2matrix_reshape
+
+  function c_matrix2tensor_reshape(matrix,Norb) result(tensor)
+    implicit none
+    integer                                     :: Norb
+    complex(8),dimension(Norb,Norb,Norb,Norb,2) :: tensor
+    complex(8),dimension(Norb*Norb,Norb*Norb,2) :: matrix
+    integer                                     :: iorb,jorb,korb,lorb
+    integer                                     :: io,jo,spin
+    tensor = zero
+    do io=1,Norb
+       do jo=1,Norb
+          !
+          call getMstride(io,iorb,jorb,Norb)
+          call getMstride(jo,korb,lorb,Norb)
+          !
+          do spin=1,2
+             matrix(io,jo,spin) = tensor(iorb,jorb,korb,lorb,spin)
+          enddo
+          !
+       enddo
+    enddo
+  end function c_matrix2tensor_reshape
+
+  function d_matrix2tensor_reshape(matrix,Norb) result(tensor)
+    implicit none
+    integer                                     :: Norb
+    real(8),dimension(Norb,Norb,Norb,Norb,2)    :: tensor
+    real(8),dimension(Norb*Norb,Norb*Norb,2)    :: matrix
+    integer                                     :: iorb,jorb,korb,lorb
+    integer                                     :: io,jo,spin
+    tensor = 0d0
+    do io=1,Norb
+       do jo=1,Norb
+          !
+          call getMstride(io,iorb,jorb,Norb)
+          call getMstride(jo,korb,lorb,Norb)
+          !
+          do spin=1,2
+             matrix(io,jo,spin) = tensor(iorb,jorb,korb,lorb,spin)
+          enddo
+          !
+       enddo
+    enddo
+  end function d_matrix2tensor_reshape
+
+
+  subroutine getUstride(ndx,iorb,jorb,Norb)
+    implicit none
+    integer,intent(in)                          :: iorb,jorb,Norb
+    integer,intent(out)                         :: ndx
+    integer,dimension(Norb,Norb)                :: stride
+    integer                                     :: count
+    integer                                     :: io,jo
+    !
+    count=0
+    do io=1,Norb
+       count=count+1
+       stride(io,io)=count
+    enddo
+    do io=1,Norb
+       do jo=io+1,Norb
+          count=count+1
+          stride(io,jo)=count
+          count=count+1
+          stride(jo,io)=count
+       enddo
+    enddo
+    !
+    ndx = stride(iorb,jorb)
+    !
+  end subroutine getUstride
+
+  subroutine getMstride(ndx,iorb,jorb,Norb)
+    implicit none
+    integer,intent(in)                          :: ndx,Norb
+    integer,intent(out)                         :: iorb,jorb
+    integer,dimension(Norb,Norb)                :: stride
+    integer                                     :: count
+    integer                                     :: io,jo
+    !
+    count=0
+    do io=1,Norb
+       count=count+1
+       stride(io,io)=count
+    enddo
+    do io=1,Norb
+       do jo=io+1,Norb
+          count=count+1
+          stride(io,jo)=count
+          count=count+1
+          stride(jo,io)=count
+       enddo
+    enddo
+    !
+    strideloop:do iorb=1,Norb
+       do jorb=1,Norb
+          if(stride(iorb,jorb)==ndx) exit strideloop
+       enddo
+    enddo strideloop
+    !
+end subroutine getMstride
+
+
+
+end module DMFT_INTERACTION
---
+++ src/DMFT_INTERACTION/dmft_interaction_setKanamori_mpi.f90
@@ -0,0 +1,76 @@
+subroutine dmft_interaction_setKanamori_mpi(MpiComm,Utensor,test)
+  implicit none
+  integer                                       :: MpiComm
+  real(8),dimension(:,:,:,:,:),intent(inout)    :: Utensor  ! [Norb][Norb][Norb][Norb][2]
+  logical,intent(in),optional                   :: test
+  !
+  integer                                       :: Norb
+  real(8),dimension(3)                          :: Uloc
+  real(8)                                       :: Ust,Jh,Jx,Jp
+  !
+  integer                                       :: iorb,jorb,korb,lorb
+  integer                                       :: io,jo
+  !
+  !MPI setup:
+  mpi_size  = get_size_MPI(MpiComm)
+  mpi_rank =  get_rank_MPI(MpiComm)
+  mpi_master= get_master_MPI(MpiComm)
+  !
+  !Retrieve parameters:
+  call get_ctrl_var(Norb,"NORB")
+  call get_ctrl_var(Uloc,"ULOC")
+  call get_ctrl_var(Ust,"ULOC")
+  call get_ctrl_var(Jh,"JH")
+  call get_ctrl_var(Jx,"JX")
+  call get_ctrl_var(Jp,"JP")
+  !
+  !
+  if(mpi_master)then
+     !
+     !Testing part:
+     call assert_shape(Utensor,[Norb,Norb,Norb,Norb,2],"dmft_interaction_setKanamori_mpi","Umat")
+     !
+     Utensor=0d0
+     !
+     !density-density interaction: same orbital, opposite spins
+     do iorb=1,Norb
+        Utensor(iorb,iorb,iorb,iorb,1) = Uloc(iorb)
+     enddo
+     !
+     !density-density interaction: different orbitals, opposite spins
+     do iorb=1,Norb
+        do jorb=1+iorb,Norb
+           Utensor(iorb,iorb,jorb,jorb,1) = Ust
+           Utensor(jorb,jorb,iorb,iorb,1) = Ust
+        enddo
+     enddo
+     !
+     !density-density interaction: different orbitals, parallel spins
+     do iorb=1,Norb
+        do jorb=1+iorb,Norb
+           Utensor(iorb,iorb,jorb,jorb,2) = (Ust-Jh)/2.d0
+           Utensor(jorb,jorb,iorb,iorb,2) = (Ust-Jh)/2.d0
+        enddo
+     enddo
+     !
+     !spin-exchange and pair-hopping
+     do iorb=1,Norb
+        do jorb=1+iorb,Norb
+           Utensor(iorb,jorb,jorb,iorb,1) = Jx
+           Utensor(iorb,jorb,iorb,jorb,1) = Jp
+        enddo
+     enddo
+     !
+     ! TEST-ME most probably useless due to non existence of connecting states
+     if(test)then
+        do iorb=1,Norb
+           do jorb=1+iorb,Norb
+              Utensor(iorb,jorb,jorb,iorb,2) = -(Ust-Jh)/2.d0
+           enddo
+        enddo
+     endif
+     !
+  endif
+  call Bcast_Mpi(MpiComm,Utensor)
+  !
+end subroutine dmft_interaction_setKanamori_mpi
